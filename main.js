c9144b1ecb70b8260880a33bb552b2fb8d96f25f
DanielBrittainJr <djbrittain2017@gmail.com>
2023-01-15 04:37:28 -0500

    the enemy will now follow the player

--- main.js
+++ main.js
@@ -19,6 +19,8 @@ function drawEnemy(x, y) {
     context.fillRect(x, y, enemy.width, enemy.height);
 }
 
+
+//move player
 document.addEventListener("keydown", function(event) {
     if (event.code === "ArrowUp") {
         player.y -= 5;
@@ -31,10 +33,23 @@ document.addEventListener("keydown", function(event) {
     }
 });
 
+let enemySpeed = 0.5;
+
 
 function update() {
     //clear canvas
     context.clearRect(0, 0, canvas.width, canvas.height);
+
+    //getting 2 points (x,y) for theta value or "angle"
+    let angle = Math.atan2(player.y - enemy.y, player.x - enemy.x);
+
+    //pass theta or angle to cos and sin function to get number between range [-1,1] for both x and y
+    //Y = sin, X = cos
+    enemVelocity = { x: Math.cos(angle), y: Math.sin(angle) };
+
+    //move enemy towards player
+    enemy.x += enemVelocity.x * enemySpeed;
+    enemy.y += enemVelocity.y * enemySpeed;
 
 
     drawPlayer(player.x, player.y);
